d4025832a7a6a0c98eba4dce141a59344429f6e6
Thaniyell <essien.nathan@yahoo.com>
2022-10-06 13:39:37 +0100

    Fully Functional

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -3,40 +3,42 @@ import React, { useState } from 'react'
 const Child = (props) => {
   const [value, setValue] = useState('')
   const [display, setDisplay] = useState(false)
-  const handleChange = (event) => {
-    setValue(event.target.value)
-    // console.log(event.target.value)
-  }
-  function wordChanger() {
-    if (value === '') {
-      setDisplay((prevDisplay) => {
-        return !prevDisplay
-      })
-    } else {
-         const newInput = {
-        id: Math.random().toString(),
-        title: value
-      }
-      props.newProps(newInput)
 
+  const handleChange = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setDisplay(false)
     }
+    setValue(event.target.value)
+  };
+
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (value.trim().length === 0) {
+      setDisplay((prevState) => {
+        return !prevState
+      })
+      return;
+    }
+    const newInput = {
+      id: Math.random().toString(),
+      title: value
+    }
+
+    props.newProps(newInput)
     setValue('')
-  }
-  function doNothing() {
-    setDisplay((prevDisplay) => {
-      return !prevDisplay
-    })
-  }
+  };
+
 
   return (
-    <div className='todo'>
+    <form className='todo' onSubmit={formSubmitHandler}>
       <div>
-        <label>Please enter a Todo Below</label>
-        <input type='text' onChange={handleChange} value={value} />
-       {display && <p className='error'>Please add a Todo</p>}
-        <button className='btn' onClick={display ? doNothing : wordChanger}>Add Todo</button></div>
+        <label style={{ color: display && 'red' }}>Please enter a Todo Below</label>
+        <input type='text' onChange={handleChange} value={value} style={{ borderColor: display && 'red' }} />
+        <button className='btn' type='submit'>Add Todo</button>
+      </div>
 
-    </div>
+    </form>
 
   )
 }
