81eef7880482c5a408a24d5c85eea388a0457565
Thaniyell <essien.nathan@yahoo.com>
2022-10-06 14:09:23 +0100

    Final commit

--- src/Components/InputElements.js
+++ src/Components/InputElements.js
@@ -10,7 +10,7 @@ const InputElements = (props) => {
 
   return (
     <ul>
-      {list ? <li>{props.title}<button onClick={removeHandler}><MdClose /></button></li> : null}
+      {list && <li>{props.title}<button onClick={removeHandler}><MdClose /></button></li>}
     </ul>
   )
 }
